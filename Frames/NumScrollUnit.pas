aa7aaa094b10bc830201714a7c45656e0641cd71
AnnihilatoR <AnnihilatoR@localhost>
2025-04-05 18:15:23 +0300

    добавлен таймер

---
+++ Frames/NumScrollUnit.pas
@@ -0,0 +1,153 @@
+unit NumScrollUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, FMX.Layouts;
+
+type
+  TNumScrollFrame = class(TFrame)
+    NumsLayout: TLayout;
+    TopNumText: TText;
+    BottomNumText: TText;
+    CurrentNumText: TText;
+    BackgroundRectangle: TRectangle;
+    Line1: TLine;
+    Line2: TLine;
+    procedure NumsLayoutMouseWheel(Sender: TObject; Shift: TShiftState;
+      WheelDelta: Integer; var Handled: Boolean);
+  private
+    FMinVal: Integer;
+    FMaxVal: Integer;
+    FStepVal: Integer;
+    FCurrentVal: Integer;
+    FDimention: Integer;
+
+    function AlignNum(const AVal: Integer): String;
+    procedure SetCurrentVal(const ACurrentVal: Integer);
+    function GetCurrentValStr: String;
+    procedure SetCurrentValStr(const ACurrentValStr: String);
+
+    function ShiftVal(const AStepVal: Integer; const ADirection: Boolean): Integer;
+
+//    property MinVal: Integer read FMinVal write FMinVal;
+//    property MaxVal: Integer read FMaxVal write SetMaxVal;
+  public
+    property CurrentVal: Integer read FCurrentVal write SetCurrentVal;
+    property CurrentValStr: String read GetCurrentValStr write SetCurrentValStr;
+
+    procedure Init(
+      const AMinVal: Integer;
+      const AMaxVal: Integer;
+      const AStepVal: Integer);
+
+    function IncVal: Integer;
+    function DecVal: Integer;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+function TNumScrollFrame.AlignNum(const AVal: Integer): String;
+var
+  i: Byte;
+  ValLength: Word;
+begin
+  Result := '';
+
+  ValLength := AVal.ToString.Length;
+  if ValLength < FDimention then
+  begin
+    for i := 0 to Pred(FDimention - ValLength) do
+      Result := '0' + Result;
+  end;
+
+  Result := Result + AVal.ToString;
+end;
+
+procedure TNumScrollFrame.SetCurrentVal(const ACurrentVal: Integer);
+begin
+  if (ACurrentVal > FMaxVal) or (ACurrentVal < FMinVal) then
+    raise Exception.Create('Out of range');
+
+  FCurrentVal := ACurrentVal;
+
+  TopNumText.Text := AlignNum(ShiftVal(FStepVal, false));
+  BottomNumText.Text := AlignNum(ShiftVal(FStepVal, true));
+  CurrentNumText.Text := AlignNum(FCurrentVal);
+end;
+
+function TNumScrollFrame.GetCurrentValStr: String;
+begin
+  Result := AlignNum(CurrentVal);
+end;
+
+procedure TNumScrollFrame.SetCurrentValStr(const ACurrentValStr: String);
+begin
+  try
+    CurrentVal := ACurrentValStr.ToInteger;
+  except
+    raise;
+  end;
+end;
+
+procedure TNumScrollFrame.Init(
+  const AMinVal: Integer;
+  const AMaxVal: Integer;
+  const AStepVal: Integer);
+begin
+  FMinVal := AMinVal;
+  FMaxVal := AMaxVal;
+  FStepVal := AStepVal;
+
+  FDimention := FMaxVal.ToString.Length;
+end;
+
+procedure TNumScrollFrame.NumsLayoutMouseWheel(Sender: TObject;
+  Shift: TShiftState; WheelDelta: Integer; var Handled: Boolean);
+begin
+  if WheelDelta < 0 then
+    IncVal
+  else
+  if WheelDelta > 0 then
+    DecVal;
+end;
+
+function TNumScrollFrame.ShiftVal(
+  const AStepVal: Integer;
+  const ADirection: Boolean): Integer;
+var
+  NumVal: Integer;
+begin
+  if ADirection then
+  begin
+    NumVal := FCurrentVal + AStepVal;
+    if NumVal > FMaxVal then
+      NumVal := FMinVal + (NumVal - FMaxVal) - 1;
+  end
+  else
+  begin
+    NumVal := FCurrentVal - AStepVal;
+    if NumVal < FMinVal then
+      NumVal := (FMaxVal + 1) - (NumVal * -1);
+  end;
+
+  Result := NumVal;
+end;
+
+function TNumScrollFrame.IncVal: Integer;
+begin
+  Result := ShiftVal(FStepVal, true);
+  SetCurrentVal(Result);
+end;
+
+function TNumScrollFrame.DecVal: Integer;
+begin
+  Result := ShiftVal(FStepVal, false);
+  SetCurrentVal(Result);
+end;
+
+end.
---
+++ SetTimerFormUnit.pas
@@ -0,0 +1,89 @@
+unit SetTimerFormUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, NumScrollUnit,
+  BorderFrameUnit, FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls,
+  FMX.Objects, FMX.Effects;
+
+type
+  TSetTimerForm = class(TForm)
+    HoursNumScrollFrame: TNumScrollFrame;
+    loContent: TLayout;
+    MinutesNumScrollFrame: TNumScrollFrame;
+    OkButton: TRectangle;
+    Text1: TText;
+    TopLayout: TLayout;
+    Text2: TText;
+    Text3: TText;
+    procedure FormCreate(Sender: TObject);
+    procedure OkButtonClick(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure OkButtonMouseEnter(Sender: TObject);
+    procedure OkButtonMouseLeave(Sender: TObject);
+  private
+    FBorderFrame: TBorderFrame;
+  public
+    function GetTime: TTime;
+  end;
+
+var
+  SetTimerForm: TSetTimerForm;
+
+implementation
+
+{$R *.fmx}
+
+procedure TSetTimerForm.OkButtonClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TSetTimerForm.OkButtonMouseEnter(Sender: TObject);
+begin
+  OkButton.Fill.Color := $FF4F4F4F;
+end;
+
+procedure TSetTimerForm.OkButtonMouseLeave(Sender: TObject);
+begin
+  OkButton.Fill.Color := $00000000;
+end;
+
+procedure TSetTimerForm.FormCreate(Sender: TObject);
+begin
+  FBorderFrame :=
+    TBorderFrame.Create(
+      Self,
+      loContent,
+      'Just a clock',
+      Trunc(loContent.Width),
+      Trunc(loContent.Height),
+      $FF2A001A,
+      $FF2A001A,
+      TAlphaColorRec.Lime,
+      TAlphaColorRec.Lime);
+
+  HoursNumScrollFrame.Init(0, 23, 1);
+  HoursNumScrollFrame.CurrentVal := 0;
+  MinutesNumScrollFrame.Init(0, 59, 1);
+  MinutesNumScrollFrame.CurrentVal := 0;
+end;
+
+procedure TSetTimerForm.FormDestroy(Sender: TObject);
+begin
+  SetTimerForm := nil;
+end;
+
+function TSetTimerForm.GetTime: TTime;
+begin
+  Result :=
+    EncodeTime(
+      HoursNumScrollFrame.CurrentVal,
+      MinutesNumScrollFrame.CurrentVal,
+      0,
+      0);
+end;
+
+end.
