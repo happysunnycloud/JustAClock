63559e4234952aa5385888f37af3404c5fb4f97a
AnnihilatoR <AnnihilatoR@localhost>
2025-04-24 18:14:58 +0300

    добавление вертикального табло

---
+++ CommonUnit.pas
@@ -0,0 +1,27 @@
+﻿unit CommonUnit;
+
+interface
+
+const
+  VERTICAL_ORIENTATION_IDENT = 'Vertical';
+  HORIZONTAL_ORIENTATION_IDENT = 'Horizontal';
+
+type
+  TOrientationKind = (okHorizontal = 0, okVertical = 1);
+  TBoardKind = (bkText = 0, bkElectronic = 1);
+
+  TState = class
+  strict private
+    class var
+      FColorIdent: String;
+      FOrientation: TOrientationKind;
+      FBoard: TBoardKind;
+  public
+    class property ColorIdent: String read FColorIdent write FColorIdent;
+    class property Orientation: TOrientationKind read FOrientation write FOrientation;
+    class property Board: TBoardKind read FBoard write FBoard;
+  end;
+
+implementation
+
+end.
---
+++ LostUnits/VerticalElectronicBoardFrameUnit.pas
@@ -0,0 +1,39 @@
+﻿unit VerticalElectronicBoardFrameUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, FMX.Layouts, FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TVerticalElectronicBoardFrame = class(TFrame)
+    BackgroundLayout: TLayout;
+    BackgroundRectangle: TRectangle;
+    DigitsLayout: TLayout;
+    HHImage: TImage;
+    MHImage: TImage;
+    HLImage: TImage;
+    MLImage: TImage;
+    SLImage: TImage;
+    SHImage: TImage;
+    HDelimImage: TImage;
+    SDelimImage: TImage;
+    TimeVoidEdit: TEdit;
+    HoursLayout: TLayout;
+    MinutesLayout: TLayout;
+    SecondsLayout: TLayout;
+    HoursDelimLayout: TLayout;
+    SecondsDelimLayout: TLayout;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
