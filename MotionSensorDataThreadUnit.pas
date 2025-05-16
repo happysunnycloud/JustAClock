6d754c7f184896e6959f7d2cc7136e6341222ff6
AnnihilatoR <AnnihilatoR@localhost>
2025-05-16 23:00:15 +0300

    модуль для снятия данных с датчика движения

---
+++ MotionSensorDataThreadUnit.pas
@@ -0,0 +1,169 @@
+unit MotionSensorDataThreadUnit;
+
+interface
+
+uses
+    System.SyncObjs
+  , System.Sensors
+  , ThreadFactoryUnit
+  , FMX.FormExtUnit
+  ;
+
+type
+  TDetectOrientationProc = reference to procedure;
+
+  TMotionSensorDataThread = class(TThreadExt)
+  strict private
+//    FCriticalSection: TCriticalSection;
+
+    FVerticalDetectedProc: TDetectOrientationProc;
+    FHorizontalDetectedProc: TDetectOrientationProc;
+    FSensor: TCustomMotionSensor;
+
+  protected
+    // —пециально не перегружаем Execute,
+    // чтобы выполнилс€ на стороне родительского класса
+    // ¬ родителе лов€тс€ исключени€
+    procedure Execute(const AThread: TThreadExt); reintroduce; // override;
+  public
+    constructor Create(
+      const AThreadFactory: TThreadFactory;
+      const ASensor: TCustomMotionSensor;
+      const AVerticalDetectedProc: TDetectOrientationProc;
+      const AHorizontalDetectedProc: TDetectOrientationProc);
+    destructor Destroy; override;
+
+    class procedure Init(
+      const AForm: TFormExt;
+      const AVerticalDetectedProc: TDetectOrientationProc;
+      const AHorizontalDetectedProc: TDetectOrientationProc);
+  end;
+
+implementation
+
+uses
+    System.SysUtils
+  , System.Math
+  ;
+
+constructor TMotionSensorDataThread.Create(
+  const AThreadFactory: TThreadFactory;
+  const ASensor: TCustomMotionSensor;
+  const AVerticalDetectedProc: TDetectOrientationProc;
+  const AHorizontalDetectedProc: TDetectOrientationProc);
+begin
+//  FCriticalSection := TCriticalSection.Create;
+
+  FSensor := ASensor;
+
+  FVerticalDetectedProc := AVerticalDetectedProc;
+  FHorizontalDetectedProc := AHorizontalDetectedProc;
+
+  FreeOnTerminate := true;
+
+  inherited Create(
+    AThreadFactory,
+    'TMotionSensorDataThread',
+    Self.Execute);
+end;
+
+destructor TMotionSensorDataThread.Destroy;
+begin
+//  if Assigned(FCriticalSection) then
+//    FreeAndNil(FCriticalSection);
+
+  inherited;
+end;
+
+class procedure TMotionSensorDataThread.Init(
+  const AForm: TFormExt;
+  const AVerticalDetectedProc: TDetectOrientationProc;
+  const AHorizontalDetectedProc: TDetectOrientationProc);
+
+  function _HasSensorProperty(
+    const ASensor: TCustomMotionSensor;
+    const AProperty: TCustomMotionSensor.TProperty): Boolean;
+  var
+    SensorProperty: TCustomMotionSensor.TProperty;
+  begin
+    Result := false;
+
+    for SensorProperty in ASensor.AvailableProperties do
+    begin
+      if SensorProperty = AProperty then
+        Exit(true);
+    end;
+  end;
+
+var
+  SensorManager: TSensorManager;
+  Sensors: TSensorArray;
+  Sensor: TCustomMotionSensor;
+  HasProperties: Boolean;
+begin
+  ReportMemoryLeaksOnShutdown := true;
+
+  SensorManager := TSensorManager.Current;
+  SensorManager.Activate;
+
+  Sensors := TSensorManager.Current.GetSensorsByCategory(TSensorCategory.Motion);
+  if Length(Sensors) = 0 then
+    Exit;
+
+  Sensor := TCustomMotionSensor(Sensors[0]);
+
+  HasProperties :=
+    _HasSensorProperty(Sensor, TCustomMotionSensor.TProperty.AccelerationX) and
+    _HasSensorProperty(Sensor, TCustomMotionSensor.TProperty.AccelerationY) and
+    _HasSensorProperty(Sensor, TCustomMotionSensor.TProperty.AccelerationX);
+
+  if not HasProperties then
+    Exit;
+
+  AForm.ThreadFactory.CreateRegistredThread(
+    procedure (
+      const AThreadFactory: TThreadFactory)
+    begin
+      TMotionSensorDataThread.Create(
+        AThreadFactory,
+        Sensor,
+        AVerticalDetectedProc,
+        AHorizontalDetectedProc);
+    end);
+end;
+
+procedure TMotionSensorDataThread.Execute(const AThread: TThreadExt);
+var
+  AccelerationX: Double;
+  AccelerationY: Double;
+begin
+  FSensor.Start;
+
+  while not Terminated do
+  begin
+    AccelerationX := FSensor.AccelerationX;
+    AccelerationY := FSensor.AccelerationY;
+
+    if (Abs(RoundTo(AccelerationX, -2)) > 0.5) and
+       (Abs(RoundTo(AccelerationY, -2)) < 0.5)
+    then
+      TThreadExt.Queue(nil,
+        procedure
+        begin
+          FHorizontalDetectedProc;
+        end)
+    else
+      TThreadExt.Queue(nil,
+        procedure
+        begin
+          FVerticalDetectedProc;
+        end);
+
+    Sleep(600);
+  end;
+
+  FSensor.Stop;
+end;
+
+
+end.
